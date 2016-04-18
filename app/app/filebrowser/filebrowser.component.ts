d7c630ba56b7247020cf5fc019711ff8cd6a06ec
Yogesh Dhanapal <geoyogesh@gmail.com>
2016-04-17 20:14:52 -0400

    fix

--- app/app/filebrowser/filebrowser.component.ts
+++ app/app/filebrowser/filebrowser.component.ts
@@ -1,14 +1,16 @@
 import {Component} from 'angular2/core';
 
-
 @Component({
     selector: 'filebrowser-component',
     template: `<h1>this is file browser component</h1>
 <div>{{path}}</div>
 <div class='simplestyle'>hello</div>
-    `
+    <button [disabled]="ismedia_selected !== true">Cast</button>
+    `,
+    styleUrls: ['app/filebrowser/style.css']
 })
 
 export class FileBrowserComponent { 
     path = '/';
+    ismedia_selected: false;
 }
